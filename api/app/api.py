f3127cbe15c750e9ecb4df7749125dd9c6a205e2
Ben Falk <falk.ben@gmail.com>
2022-11-07 18:59:27 -0500

    m2m relationship with genres working

--- api/app/api.py
+++ api/app/api.py
@@ -3,7 +3,7 @@
 from typing import List
 
 from app import tables
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 from sqlmodel import Session, SQLModel, create_engine, select
 
 sqlite_file_name = "database.db"
@@ -13,8 +13,26 @@ connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 
 
+# todo: refactor to db helpers
+# todo: add correct types for model
+def get_or_create(session: Session, model, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance
+    else:
+        instance = model(**kwargs)
+        session.add(instance)
+        session.flush()
+        return instance
+
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 
 app = FastAPI()
@@ -26,41 +44,62 @@ def on_startup():
 
 
 @app.post("/movie/", response_model=tables.MovieRead)
-def create_movie(movie: tables.MovieCreate):
-    with Session(engine) as session:
-        db_movie = tables.Movie.from_orm(movie)
-        session.add(db_movie)
-        session.commit()
-        session.refresh(db_movie)
-        return db_movie
+def create_movie(movie: tables.MovieCreate, session: Session = Depends(get_session)):
+
+    genres = movie.genres
+    db_movie = tables.Movie.from_orm(movie)
+
+    # adding genres to movie
+    db_genres = []
+    for genre in genres:
+        db_genres.append(get_or_create(session, tables.Genre, name=genre))
+    db_movie.genres = db_genres
+
+    session.add(db_movie)
+    session.commit()
+    session.refresh(db_movie)
+
+    return db_movie
 
 
 @app.get("/movies/", response_model=List[tables.MovieRead])
-def list_movies():
-    with Session(engine) as session:
-        movies = session.exec(select(tables.Movie)).all()
-        return movies
+def list_movies(session: Session = Depends(get_session)):
+    movies = session.exec(select(tables.Movie)).all()
+    return movies
 
 
 @app.get("/movie/{movie_id}", response_model=tables.MovieRead)
-def read_movie(movie_id: int):
-    with Session(engine) as session:
-        movie = session.get(tables.Movie, movie_id)
-        if not movie:
-            raise HTTPException(status_code=404, detail="Movie not found")
-        return movie
+def read_movie(movie_id: int, session: Session = Depends(get_session)):
+    movie = session.get(tables.Movie, movie_id)
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    return movie
 
 
 @app.patch("/movie/{movie_id}", response_model=tables.MovieRead)
-def update_movie(movie_id: int, movie: tables.MovieUpdate):
-    with Session(engine) as session:
-        db_movie = session.get(tables.Movie, movie_id)
-        if not db_movie:
-            raise HTTPException(status_code=404, detail="Movie not found")
-        movie_data = movie.dict(exclude_unset=True)
-        for key, value in movie_data.items():
-            setattr(db_movie, key, value)
-        session.add(db_movie)
-        session.commit()
-        session.refresh(db_movie)
-        return db_movie
+def update_movie(
+    movie_id: int, movie: tables.MovieUpdate, session: Session = Depends(get_session)
+):
+
+    db_movie = session.get(tables.Movie, movie_id)
+    if not db_movie:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    movie_data = movie.dict(exclude_unset=True)
+    genres = movie_data.pop("genres", None)
+    for key, value in movie_data.items():
+        setattr(db_movie, key, value)
+
+    # adding genres to movie
+    if genres is not None:
+        db_genres = []
+        for genre in genres:
+            db_genres.append(get_or_create(session, tables.Genre, name=genre))
+        db_movie.genres = db_genres
+
+    session.add(db_movie)
+    session.commit()
+    session.refresh(db_movie)
+    return db_movie
+
+
+# todo: delete
--- api/app/tables.py
+++ api/app/tables.py
@@ -2,22 +2,34 @@ from datetime import datetime
 
 from sqlalchemy import Column, DateTime
 from sqlalchemy.sql import func
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
+# todo User
 # todo: Watched (date) is a M2M with Users and Movies
+
+
+class GenreMovieLink(SQLModel, table=True):
+    genre_id: int | None = Field(default=None, foreign_key="genre.id", primary_key=True)
+    movie_id: int | None = Field(default=None, foreign_key="movie.id", primary_key=True)
+
+
+class Genre(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    movies: list["Movie"] = Relationship(
+        back_populates="genres", link_model=GenreMovieLink
+    )
 
 
 class MovieBase(SQLModel):
     title: str = Field(default=..., index=True)
+    # todo: add "gt" constraint on database
     year: int = Field(default=..., index=True, gt=1878)
     runtime: int = Field(default=..., index=True)
     url: str | None = Field(default=None, description="imdb url")
     img: str | None = Field(default=None, description="url to image")
     rating: str | None = Field(default=None, description="MPAA rating")
     nsfw: bool = False
-
-    # todo: created by user
-    # todo: M2M column for Genres
 
 
 class Movie(MovieBase, table=True):
@@ -28,20 +40,27 @@ class Movie(MovieBase, table=True):
     updated_at: datetime | None = Field(
         sa_column=Column(DateTime(timezone=True), onupdate=func.now())
     )
+    # todo: cascade on delete (sa_relationship_kwargs)
+    genres: list[Genre] = Relationship(
+        back_populates="movies", link_model=GenreMovieLink
+    )
+    # todo: created_by (user)
 
 
 class MovieCreate(MovieBase):
-    pass
+    genres: list[str] = []
 
 
 class MovieRead(MovieBase):
     id: int
     created_at: datetime
     updated_at: datetime | None
+    genres: list[Genre] = []
 
 
-class MovieUpdate(SQLModel):
+class MovieUpdate(MovieBase):
     title: str | None = None
     year: int | None = Field(default=None, gt=1878)
     runtime: int | None = None
     nsfw: bool | None = None
+    genres: list[str] | None = None
